82d5d7df4638195dd6531b76d937b616bcbb4716
byzze <byzze@qq.com>
2025-10-25 23:32:42 +0800

    更新wails3版本

--- frontend/dist/assets/translate-602c80f0.js
+++ frontend/dist/assets/translate-602c80f0.js
@@ -1,4 +1,4 @@
-import { c as client, j as jsxRuntimeExports, R as React, A as App } from "./index-a20acfa7.js";
+import { c as client, j as jsxRuntimeExports, R as React, A as App } from "./index-9ef97990.js";
 client.createRoot(document.getElementById("root")).render(
   /* @__PURE__ */ jsxRuntimeExports.jsx(React.StrictMode, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(App, { variable: "translate" }) })
 );
